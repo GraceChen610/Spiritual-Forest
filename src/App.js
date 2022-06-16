4f21bdde0fae359d5c138578d60db526554a8f69
Grace <100032083+GraceChen610@users.noreply.github.com>
2022-06-16 22:04:20 +0800

    Revert "Feature: setting database & quiz feature"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Quiz from './pages/quiz';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Quiz />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      ok~~~~~~
+    </div>
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
-import {
-  getFirestore, doc, addDoc, getDocs, getDoc, collection,
-} from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-export const firebaseConfig = initializeApp({
+const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
@@ -14,41 +14,10 @@ export const firebaseConfig = initializeApp({
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-});
-
-// const app = initializeApp(firebaseConfig);
-// eslint-disable-next-line no-unused-vars
-const analytics = getAnalytics(firebaseConfig);
-
-const db = getFirestore(firebaseConfig);
-
-const firebaseStores = {
-  // 讀取資料集
-  async getData(collectionName) {
-    const data = await getDocs(collection(db, collectionName));
-    return data.docs;
-  },
-
-  // 讀取單個文件
-  async getOneDoc(collectionName, collectionID) {
-    const docRef = doc(db, collectionName, collectionID);
-    const docSnap = await getDoc(docRef);
-
-    if (docSnap.exists()) {
-      console.log('Document data:', docSnap.data());
-    } else {
-      // doc.data() will be undefined in this case
-      console.log('No such document!');
-    }
-  },
-
-  // 新增資料集
-  async postdata(collectionName, content) {
-    const docRef = await addDoc(collection(db, collectionName), {
-      content,
-    });
-    console.log('Document written with ID: ', docRef.id);
-  },
 };
 
-export default firebaseStores;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+// eslint-disable-next-line no-console
+console.log(analytics);
